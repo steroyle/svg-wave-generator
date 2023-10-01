66b8f886206fe30ce048f527707bacf972ee1491
steroyle <steroyle@gmail.com>
2023-10-01 22:08:30 +0100

    Updated page title

--- src/components/SvgOptions.jsx
+++ src/components/SvgOptions.jsx
@@ -18,7 +18,7 @@ const SvgOptions = ({ optionsData, onFieldChange }) => {
             <label class="block text-gray-700 text-sm font-bold" for="height">Height</label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="height" name="height" type="number" value={optionsData.height} onChange={handleInputChange} />
           </div>
-          <div class="grid grid-cols-2 items-center mb-4 gap-0">
+          <div class="grid grid-cols-2 items-center gap-0">
             <label class="block text-gray-700 text-sm font-bold" for="height">Fill Colour</label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fill" name="fill" type="text" value={optionsData.fill} onChange={handleInputChange} />
           </div>
